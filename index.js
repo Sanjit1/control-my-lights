ba3969c0a92e30d4eca9368ef8644f56f2a520cd
Sanjit1 <sanjitsarda4@gmail.com>
2021-03-06 22:58:22 -0800

    Update index.js

--- index.js
+++ index.js
@@ -38,9 +38,10 @@ client.on('message', message => {
         + (individual_string.includes('messages') ? 1 : 0)
         + (individual_string.includes('sifu') ? 1 : 0);
 
-    if (number > 3 && message.author.id == '542199842278211594') {
+    if (number > 3 && message.author.id !== '542199842278211594') {
+        //console.log('f')
         message.channel.send('Do you think this is funny Anthony? Just copying me? Fuck you');
-        message.channel.send('afuck you <@!542199842278211594>');
+        message.channel.send('afuck you <@!542199842278211594> ');
     }
     user_id = message.author.id;
     if (messageString == null) {
